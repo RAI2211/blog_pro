565ed17cdc7e9cbcd9ed948a2d537e9dae813359
Yatendra Rai <136678809+RAI2211@users.noreply.github.com>
2023-08-31 01:23:20 +0530

    my pro3

---
+++ server/database/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const Connection = async (username, password) => {
+    const URL =`mongodb+srv://yatendra2211:xlm6hDmbpTNrlUaN@yatendra.jugsuhj.mongodb.net/yatendra?retryWrites=true&w=majority`;
+    try {
+        await mongoose.connect(URL, { useNewUrlParser: true })
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting to the database ', error);
+    }
+};
+
+export default Connection;
